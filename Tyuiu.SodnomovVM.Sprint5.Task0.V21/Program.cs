7cc2c0cc7cc5846b7e7337c075c431dd78e0ef43
tyuiu.sodnomovvm@outlook.com <deek.ren.gg@gmail.com>
2023-11-19 22:20:13 +0500

    Add project files.

---
+++ Tyuiu.SodnomovVM.Sprint5.Task0.V21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SodnomovVM.Sprint5.Task0.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
