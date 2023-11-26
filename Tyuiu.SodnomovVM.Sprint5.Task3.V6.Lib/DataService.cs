1cd5113c41724f2aa4c43182336a138a5d42a557
tyuiu.sodnomovvm@outlook.com <deek.ren.gg@gmail.com>
2023-11-26 22:52:15 +0500

    Task3

---
+++ Tyuiu.SodnomovVM.Sprint5.Task3.V6.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+
+namespace Tyuiu.SodnomovVM.Sprint5.Task3.V6.Lib
+{
+    public class DataService : ISprint5Task3V6
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask3.bin";
+            double res = (x) / Math.Pow((Math.Pow(x, 2) + x), 0.5);
+            res = Math.Round(res, 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(res));
+            }
+            return path;
+        }
+    }
+}
